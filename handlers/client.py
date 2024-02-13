286eb41e5cc93707aa37afb78223f350d9729808
AlexandriaBricoz <145438833+AlexandriaBricoz@users.noreply.github.com>
2024-02-13 15:53:25 +0500

    Добавил кнопочек

--- handlers/client.py
+++ handlers/client.py
@@ -4,7 +4,7 @@ from aiogram import types, Dispatcher
 from aiogram.dispatcher.filters import Text
 
 from create_bot import bot, bot_address, dp
-from keyboards.client_kb import keyboard, kb_client, kb_client_1, back_keyboard
+from keyboards.client_kb import keyboard, kb_client, kb_client_1, back_keyboard_1, back_keyboard_2, back_keyboard_3
 from school_database import sqlite_db
 
 """Хендлеры для взаимодействия с клиентом
@@ -26,32 +26,41 @@ async def start_bot(message: types.Message):
     # await message.reply(f'Пожалуйста напишите боту в ЛС: {bot_home}')
 
 
-@dp.callback_query_handler(lambda c: c.data.startswith('button'))
+@dp.callback_query_handler(lambda c: c.data.startswith('tariff'))
 async def process_callback(callback_query: types.CallbackQuery):
     await callback_query.answer()
     data = callback_query.data
-    if data == "button1":
+    if data == "tariff_1":
         await callback_query.message.reply("Бесплатный урок - это отличная возможность познакомиться с моим "
-                                           "стилем ведения занятий и убедиться, что йога подходит именно вам!")
-    elif data == "button2":
+                                           "стилем ведения занятий и убедиться, что йога подходит именно вам!",
+                                           reply_markup=back_keyboard_1)
+    elif data == "tariff_2":
         await callback_query.message.reply("Курс для новичков - идеальный выбор для тех, кто только начинает "
                                            "свой путь в йоге. Мы погружаемся в основы практики и сосредотачиваемся"
                                            " на укреплении основ.",
-                                           reply_markup=back_keyboard)
-    elif data == "button3":
-        await callback_query.message.reply("Онлайн-клуб - это возможность участвовать в онлайн-тренировках, "
+                                           reply_markup=back_keyboard_2)
+    elif data == "tariff_3":
+        await callback_query.message.reply("Клуб - это возможность участвовать в онлайн-тренировках, "
                                            "получать доступ к эксклюзивным материалам и общаться с другими"
-                                           " участниками виртуального сообщества.")
-    elif data == "button4":
-        await callback_query.message.reply("Офлайн-клуб - для тех, кто предпочитает живые занятия. Присоединяйтесь "
-                                           "к нашему клубу и наслаждайтесь занятиями в атмосфере"
-                                           " спокойствия и гармонии.")
-    elif data == "button5":
+                                           " участниками виртуального сообщества.",
+                                           reply_markup=back_keyboard_3)
+    if data == "tariff_1_1":
+        await callback_query.message.reply("Бесплатный урок - это отличная возможность познакомиться с моим "
+                                           "стилем ведения занятий и убедиться, что йога подходит именно вам!",
+                                           reply_markup=back_keyboard_1)
+    elif data == "tariff_2_1":
+        await callback_query.message.reply("Курс для новичков - идеальный выбор для тех, кто только начинает "
+                                           "свой путь в йоге. Мы погружаемся в основы практики и сосредотачиваемся"
+                                           " на укреплении основ.",
+                                           reply_markup=back_keyboard_2)
+    elif data == "tariff_3_1":
+        await callback_query.message.reply("Отличный выбор, переходим к оплате", reply_markup=kb_client_1)
+    elif data == "tariff_3_2":
+        await callback_query.message.reply("Отличный выбор, переходим к оплате", reply_markup=kb_client_1)
+    elif data == "tariff_0":
         await callback_query.message.reply(
-                               f'🗓 Выберите свой тарифный план👇👋',
-                               reply_markup=keyboard)
-    elif data == "button6":
-        await callback_query.message.reply("Отличный выбор, переходим к оплате", reply_markup=kb_client_1)       # ЛЮТЫЙ БАГ
+            f'🗓 Выберите свой тарифный план👇👋',
+            reply_markup=keyboard)
     else:
         await callback_query.message.reply("Произошла ошибка. Пожалуйста, попробуйте еще раз.")
 
@@ -82,7 +91,7 @@ async def get_contacts(message: types.Message):
 
 
 @dp.message_handler(Text(equals='Оплатить', ignore_case=True))
-async def set_tariff(message: types.Message,):
+async def set_tariff(message: types.Message, ):
     sqlite_db.add_subscription(str(message.from_user.id), str(message.from_user.username),
                                str(message.from_user.full_name),
                                datetime.datetime.now().date(),
--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -18,20 +18,29 @@ kb_client_1.add(but1)
 keyboard = types.InlineKeyboardMarkup()
 
 # Добавляем кнопки на клавиатуру
-button1 = types.InlineKeyboardButton(text="Бесплатный урок", callback_data="button1")
-button2 = types.InlineKeyboardButton(text="Курс для новичков", callback_data="button2")
-button3 = types.InlineKeyboardButton(text="Онлайн-клуб", callback_data="button3")
-button4 = types.InlineKeyboardButton(text="Офлайн-клуб", callback_data="button4")
+button1 = types.InlineKeyboardButton(text="Бесплатный урок", callback_data="tariff_1")
+button2 = types.InlineKeyboardButton(text="Курс для новичков", callback_data="tariff_2")
+button3 = types.InlineKeyboardButton(text="Вступить в клуб", callback_data="tariff_3")
 # Добавляем кнопки на клавиатуру в виде списка
 keyboard.add(button1)
 keyboard.add(button2)
 keyboard.add(button3)
-keyboard.add(button4)
 
+#######################################################################################################
 
-back_keyboard = types.InlineKeyboardMarkup()
-back_button = types.InlineKeyboardButton(text="Назад", callback_data="button5")
-back_button_1 = types.InlineKeyboardButton(text="Купить подписку на месяц", callback_data="button6")
+back_keyboard_1 = types.InlineKeyboardMarkup()
+back_button = types.InlineKeyboardButton(text="Назад", callback_data="tariff_0")
+back_button_1 = types.InlineKeyboardButton(text="Купить подписку на месяц_1", callback_data="tariff__1_1")
 # Добавляем кнопки на клавиатуру в виде списка
-back_keyboard.add(back_button).add(back_button_1)
+back_keyboard_1.add(back_button).add(back_button_1)
 
+back_keyboard_2 = types.InlineKeyboardMarkup()
+back_button_2_1 = types.InlineKeyboardButton(text="Купить подписку на месяц_2", callback_data="tariff_2_1")
+# Добавляем кнопки на клавиатуру в виде списка
+back_keyboard_2.add(back_button).add(back_button_2_1)
+
+back_keyboard_3 = types.InlineKeyboardMarkup()
+back_button_3_1 = types.InlineKeyboardButton(text="Купить подписку на месяц_3_1", callback_data="tariff_3_1")
+back_button_3_2 = types.InlineKeyboardButton(text="Купить подписку на месяц_3_2", callback_data="tariff_3_2")
+# Добавляем кнопки на клавиатуру в виде списка
+back_keyboard_3.add(back_button).add(back_button_3_1).add(back_button_3_2)
